0f737d7b405087f1f5e631b8782964b262710e6e
Smaker <admin@smaker.kr>
2017-04-01 19:21:36 +0900

    Update file 'index.js' - Add body-parser & Thinking API

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ var app = express();
 var http = require('http');
 http.createServer(app);
 
+var body_parser = require('body-parser');
+app.use(body_parser.json()); // support json encoded bodies
+app.use(body_parser.urlencoded({
+    extended: true
+})); // support encoded bodies
+
+
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('RespectYO');
 });
+
+app.post('/register', function (req, res) {
+    var age = req.body.age;
+    var gender = req.body.gender;
+});
+
+app.post('/board/:category', function (req, res) {
+    var category = req.params.category;
+});
+
 
 app.listen(8000, function () {
     console.log("Listenning \t :8000");
