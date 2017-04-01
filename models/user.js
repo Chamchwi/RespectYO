d8b39dc3ae2b4fc64df5320310ac5c50a62d66bb
Smaker <admin@smaker.kr>
2017-04-01 21:18:44 +0900

    Rename file 'User.js' to 'user.js'

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    age: Number,
+    gender: Number, // 0: men, 1: women
+    nickname: String
+});
+
+module.exports = mongoose.model('user', userSchema);
